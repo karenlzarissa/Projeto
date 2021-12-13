dd9bcc5c1ec16db15cbc9374b8185eb078d5ca59
karenlzarissa <96029585+karenlzarissa@users.noreply.github.com>
2021-12-13 14:28:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import requests
 
 r = requests.get("http://127.0.0.1:5000/Dados")
-r.status_code
-print(r.json)
+dic = r.json()
+
+print("Nome: " + dic['Nome'])
+
+print("Idade: " + dic['Idade'])
+
+print("Telefone: " + dic['Telefone'])
+
+print ("Status de requisição: ", r.status_code)
